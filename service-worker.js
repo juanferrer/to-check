4c4f224ab5d39d92e2575a62ab1006a1c6def82e
juanferrer <juanemilioferrer@outlook.com>
2018-12-29 23:22:46 +0000

    improve fetch handler

--- service-worker.js
+++ service-worker.js
@@ -35,7 +35,24 @@ self.addEventListener("fetch", function (event) {
 	//console.log(event.request.url);
 	event.respondWith(
 		caches.match(event.request).then(function (response) {
-			return response || fetch(event.request);
+			if (response) {
+				return response;     // if valid response is found in cache return it
+			} else {
+				return fetch(event.request)     //fetch from internet
+					.then(function (res) {
+						return caches.open("tocheck")
+							.then(function (cache) {
+								cache.put(event.request.url, res.clone());    //save the response for future
+								return res;   // return the fetched data
+							});
+					})
+					.catch(function (err) {       // fallback mechanism
+						return caches.open("error")
+							.then(function (cache) {
+								return cache.match("/offline.html");
+							});
+					});
+			}
 		})
 	);
 });
