897dbf15eb1b61fd642ec5d6e916f611a8d11a5b
JuanFerrer <juanemilioferrer@outlook.com>
2018-12-31 09:49:44 +0000

    WIP: Slowly adding cached files

--- service-worker.js
+++ service-worker.js
@@ -8,18 +8,18 @@ const URLS = [
 	"./index.html",
 	"./styles/style.css",
 	"./styles/boostrap.min.css",
-	// "./webfonts/open-sans.css",
+	"./webfonts/open-sans.css",
 	/*"./webfonts/mem8YaGs126MiZpBA-UFWJ0bbck.woff2",
 	"./webfonts/mem8YaGs126MiZpBA-UFUZ0bbck.woff2",
 	"./webfonts/mem8YaGs126MiZpBA-UFWZ0bbck.woff2",
 	"./webfonts/mem8YaGs126MiZpBA-UFVp0bbck.woff2",
 	"./webfonts/mem8YaGs126MiZpBA-UFWp0bbck.woff2",
 	"./webfonts/mem8YaGs126MiZpBA-UFW50bbck.woff2",
-	"./webfonts/mem8YaGs126MiZpBA-UFVZ0b.woff2",
+	"./webfonts/mem8YaGs126MiZpBA-UFVZ0b.woff2",*/
 	"./js/index.js",
 	"./3rd-party/jquery-3.3.1.slim.min.js",
 	"./3rd-party/bootstrap.min.js",
-	"./3rd-party/feather.min.js",*/
+	"./3rd-party/feather.min.js",
 ];
 
 importScripts("js/cache-polyfill.js");
