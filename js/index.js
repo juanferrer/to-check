4be5e07557c07dc694455f1012a4500bedd70e38
JuanFerrer <juanemilioferrer@outlook.com>
2019-05-16 10:56:16 +0100

    Increase version number, fix indentation and increse size of input box

--- js/index.js
+++ js/index.js
@@ -11,44 +11,44 @@ var sortKeys = false;
 
 // eslint-disable-next-line no-unused-vars
 let refresh = () => {
-	location.reload(true);
+    location.reload(true);
 };
 
 /** Open and close the side menu */
 let toggleSideMenu = () => {
-	if ($("#side-menu").attr("data-open")) {
-		hideSideMenu();
-	} else {
-		showSideMenu();
-	}
+    if ($("#side-menu").attr("data-open")) {
+        hideSideMenu();
+    } else {
+        showSideMenu();
+    }
 };
 
 let hideSideMenu = () => {
-	$("#side-menu")[0].style.left = "-250px";
-	$("#side-menu").removeAttr("data-open");
+    $("#side-menu")[0].style.left = "-250px";
+    $("#side-menu").removeAttr("data-open");
 };
 
 let showSideMenu = () => {
-	$("#side-menu")[0].style.left = "0px";
-	$("#side-menu").attr("data-open", true);
+    $("#side-menu")[0].style.left = "0px";
+    $("#side-menu").attr("data-open", true);
 };
 
 let toggleSort = () => {
-	sortKeys = !sortKeys;
+    sortKeys = !sortKeys;
 
-	$("#sort-button").toggleClass("on");
-	$("#sort-button").toggleClass("off");
+    $("#sort-button").toggleClass("on");
+    $("#sort-button").toggleClass("off");
 
-	$("#sort-button").attr("data-sort", sortKeys);
-	localStorage.setItem("sortKeys", sortKeys);
-	populateList();
+    $("#sort-button").attr("data-sort", sortKeys);
+    localStorage.setItem("sortKeys", sortKeys);
+    populateList();
 };
 
 /** Change the app theme */
 let switchTheme = () => {
-	// let switchToLightTheme = $("#theme-button").attr("data-theme") === "dark";
+    // let switchToLightTheme = $("#theme-button").attr("data-theme") === "dark";
 
-	/*if (switchToLightTheme) {
+    /*if (switchToLightTheme) {
 		$(":root").css("--main-color", "hsla(0, 0%, 98%, 1)");
 		$(":root").css("--background-color", "hsla(0, 0%, 90%, 1)");
 		$(":root").css("--semi-transparent-hover", "hsla(0, 0%, 30%, 0.3)");
@@ -69,35 +69,35 @@ let switchTheme = () => {
 		$("#dark-theme-icon").css("display", "none");
 	}*/
 
-	//let newTheme = switchToLightTheme ? "light" : "dark";
-	let oldTheme = $("input[name=theme]:checked").val();
-	let newTheme = oldTheme === "light" ? "dark" : "light";
+    //let newTheme = switchToLightTheme ? "light" : "dark";
+    let oldTheme = $("input[name=theme]:checked").val();
+    let newTheme = oldTheme === "light" ? "dark" : "light";
 
-	// $(`#${newTheme}-theme-icon`).css("display", "none");
-	// $(`#${oldTheme}-theme-icon`).css("display", "initial");
+    // $(`#${newTheme}-theme-icon`).css("display", "none");
+    // $(`#${oldTheme}-theme-icon`).css("display", "initial");
 
-	$("#theme-button").toggleClass("light");
-	$("#theme-button").toggleClass("dark");
+    $("#theme-button").toggleClass("light");
+    $("#theme-button").toggleClass("dark");
 
-	$(`input[value=${newTheme}`).click();
+    $(`input[value=${newTheme}`).click();
 
-	$("#theme-button").attr("data-theme", newTheme);
-	localStorage.setItem("currentTheme", newTheme);
+    $("#theme-button").attr("data-theme", newTheme);
+    localStorage.setItem("currentTheme", newTheme);
 };
 
 let toggleHideCompleted = () => {
-	hideCompleted = !hideCompleted;
+    hideCompleted = !hideCompleted;
 
-	// $("#hide-completed-icon").css("display", hideCompleted ? "none" : "initial");
-	// $("#show-completed-icon").css("display", hideCompleted ? "initial" : "none");
+    // $("#hide-completed-icon").css("display", hideCompleted ? "none" : "initial");
+    // $("#show-completed-icon").css("display", hideCompleted ? "initial" : "none");
 
-	$("#completed-button").toggleClass("on");
-	$("#completed-button").toggleClass("off");
+    $("#completed-button").toggleClass("on");
+    $("#completed-button").toggleClass("off");
 
-	$("#completed-button").attr("data-hide", hideCompleted);
-	localStorage.setItem("hideCompleted", hideCompleted);
+    $("#completed-button").attr("data-hide", hideCompleted);
+    localStorage.setItem("hideCompleted", hideCompleted);
 
-	populateList();
+    populateList();
 };
 
 /**
@@ -106,16 +106,16 @@ let toggleHideCompleted = () => {
  * @listens blur
  */
 let updateListTitle = e => {
-	let newListName = e.currentTarget.textContent;
-	if (convertToVarName(newListName, true) !== currentListName) {
-		lists[convertToVarName(newListName, true)] = lists[currentListName];
-		lists[currentListName] = undefined;
-		currentListName = convertToVarName(newListName, true);
+    let newListName = e.currentTarget.textContent;
+    if (convertToVarName(newListName, true) !== currentListName) {
+        lists[convertToVarName(newListName, true)] = lists[currentListName];
+        lists[currentListName] = undefined;
+        currentListName = convertToVarName(newListName, true);
 
-		saveLists();
-		populateList();
-		populateSideMenu();
-	}
+        saveLists();
+        populateList();
+        populateSideMenu();
+    }
 };
 
 /**
@@ -124,12 +124,12 @@ let updateListTitle = e => {
  * @listens click
  */
 let changeListSelection = e => {
-	currentListName = convertToVarName(e.currentTarget.firstChild.textContent, true);
+    currentListName = convertToVarName(e.currentTarget.firstChild.textContent, true);
 
-	populateList();
-	populateSideMenu();
-	saveLists();
-	toggleSideMenu();
+    populateList();
+    populateSideMenu();
+    saveLists();
+    toggleSideMenu();
 };
 
 /**
@@ -138,19 +138,19 @@ let changeListSelection = e => {
  * @listens click
  */
 let addList = () => {
-	currentListName = convertToVarName("New List", true);
+    currentListName = convertToVarName("New List", true);
 
-	let newListNum = 2;
-	while (lists[currentListName] !== undefined) {
-		currentListName = convertToVarName(`New List ${newListNum}`, true);
-		newListNum++;
-	}
+    let newListNum = 2;
+    while (lists[currentListName] !== undefined) {
+        currentListName = convertToVarName(`New List ${newListNum}`, true);
+        newListNum++;
+    }
 
-	lists[currentListName] = {};
-	populateList();
-	saveLists();
-	populateSideMenu();
-	toggleSideMenu();
+    lists[currentListName] = {};
+    populateList();
+    saveLists();
+    populateSideMenu();
+    toggleSideMenu();
 };
 
 /**
@@ -159,18 +159,18 @@ let addList = () => {
  * @listens click
  */
 let removeList = e => {
-	let listToRemove = convertToVarName(e.currentTarget.previousElementSibling.textContent, true);
+    let listToRemove = convertToVarName(e.currentTarget.previousElementSibling.textContent, true);
 
 
-	lists[listToRemove] = undefined;
-	populateSideMenu();
-	if (currentListName === listToRemove) {
-		if ($("#side-menu-list").children().length === 0) {
-			addList();
-		} else currentListName = convertToVarName($("#side-menu-list").children()[0].firstChild.textContent, true);
-	}
-	populateList();
-	saveLists();
+    lists[listToRemove] = undefined;
+    populateSideMenu();
+    if (currentListName === listToRemove) {
+        if ($("#side-menu-list").children().length === 0) {
+            addList();
+        } else currentListName = convertToVarName($("#side-menu-list").children()[0].firstChild.textContent, true);
+    }
+    populateList();
+    saveLists();
 };
 
 /**
@@ -179,14 +179,14 @@ let removeList = e => {
  * @listens click
  * */
 let toggleCheckbox = e => {
-	let checkbox = e.currentTarget.previousElementSibling;
-	checkbox.checked = !checkbox.checked;
+    let checkbox = e.currentTarget.previousElementSibling;
+    checkbox.checked = !checkbox.checked;
 
-	if (checkbox.checked) checkbox.setAttribute("checked", true);
-	else checkbox.removeAttribute("checked");
+    if (checkbox.checked) checkbox.setAttribute("checked", true);
+    else checkbox.removeAttribute("checked");
 
-	saveLists();
-	populateList();
+    saveLists();
+    populateList();
 };
 
 /**
@@ -195,15 +195,15 @@ let toggleCheckbox = e => {
  * @listens click
  */
 let addItemToList = e => {
-	let itemName = e.currentTarget.nextElementSibling.textContent;
-	if (itemName) {
-		lists[currentListName] = lists[currentListName] || {};
+    let itemName = e.currentTarget.nextElementSibling.textContent;
+    if (itemName) {
+        lists[currentListName] = lists[currentListName] || {};
 
-		lists[currentListName][convertToVarName(itemName)] = false;
-		populateList();
-		saveLists();
-		$("#list-add-input").empty();
-	}
+        lists[currentListName][convertToVarName(itemName)] = false;
+        populateList();
+        saveLists();
+        $("#list-add-input").empty();
+    }
 };
 
 /**
@@ -211,9 +211,9 @@ let addItemToList = e => {
  * @param {Event} e
  */
 let removeItem = e => {
-	e.currentTarget.parentElement.remove();
-	saveLists();
-	populateList();
+    e.currentTarget.parentElement.remove();
+    saveLists();
+    populateList();
 };
 
 /**
@@ -221,28 +221,28 @@ let removeItem = e => {
  * @param {*} e
  */
 let handleKeyPress = e => {
-	if (e.key === "Enter") $("#list-add-button").click();
+    if (e.key === "Enter") $("#list-add-button").click();
 };
 
 /**
  * Set the theme in use
  */
 let loadSettings = () => {
-	currentTheme = localStorage.getItem("currentTheme") || "light";
-	if (currentTheme !== $("#theme-button").attr("data-theme")) switchTheme();
+    currentTheme = localStorage.getItem("currentTheme") || "light";
+    if (currentTheme !== $("#theme-button").attr("data-theme")) switchTheme();
 
-	hideCompleted = localStorage.getItem("hideCompleted") === "false";
-	if (hideCompleted !== $("#completed-button").attr("data-hide")) toggleHideCompleted();
+    hideCompleted = localStorage.getItem("hideCompleted") === "false";
+    if (hideCompleted !== $("#completed-button").attr("data-hide")) toggleHideCompleted();
 
-	sortKeys = localStorage.getItem("sortKeys") || "false";
-	if (sortKeys !== $("#sort-keys").attr("data-sort")) toggleSort();
+    sortKeys = localStorage.getItem("sortKeys") || "false";
+    if (sortKeys !== $("#sort-keys").attr("data-sort")) toggleSort();
 };
 
 /** Load the lists */
 let loadLists = () => {
-	let listsJSONStr = localStorage.getItem("toCheckLists");
-	lists = listsJSONStr ? JSON.parse(listsJSONStr) : {};
-	currentListName = localStorage.getItem("currentList") || convertToVarName("New List", true);
+    let listsJSONStr = localStorage.getItem("toCheckLists");
+    lists = listsJSONStr ? JSON.parse(listsJSONStr) : {};
+    currentListName = localStorage.getItem("currentList") || convertToVarName("New List", true);
 };
 
 /**
@@ -250,8 +250,8 @@ let loadLists = () => {
  * @param {string} s String to be titleised
  */
 let convertToTitle = s => {
-	let firstCharOfName = s.startsWith(listPrefaceString) || s.startsWith(elementPrefaceString) ? elementPrefaceString.length : 0;
-	return decodeURI(s.slice(firstCharOfName));
+    let firstCharOfName = s.startsWith(listPrefaceString) || s.startsWith(elementPrefaceString) ? elementPrefaceString.length : 0;
+    return decodeURI(s.slice(firstCharOfName));
 };
 
 /**
@@ -260,111 +260,111 @@ let convertToTitle = s => {
  * @param {boolean} isList Whether the var name needs to be for an element or a list
  */
 let convertToVarName = (s, isList = false) => {
-	return (isList ? listPrefaceString : elementPrefaceString) + encodeURI(s).trim();
+    return (isList ? listPrefaceString : elementPrefaceString) + encodeURI(s).trim();
 };
 
 /** Load the list names on the side menu */
 let populateSideMenu = () => {
-	$("#side-menu-list").empty();
+    $("#side-menu-list").empty();
 
-	for (let itemName in lists) {
-		if (lists[itemName]) {
-			let newElement = document.createElement("li");
-			newElement.setAttribute("class", `list-group-item d-flex align-items-center ${currentListName === itemName ? "selected" : ""}`);
-			newElement.innerHTML = `<span class="side-menu-list-name theme-colored-text" >${convertToTitle(itemName)}</span>` +
+    for (let itemName in lists) {
+        if (lists[itemName]) {
+            let newElement = document.createElement("li");
+            newElement.setAttribute("class", `list-group-item d-flex align-items-center ${currentListName === itemName ? "selected" : ""}`);
+            newElement.innerHTML = `<span class="side-menu-list-name theme-colored-text" >${convertToTitle(itemName)}</span>` +
 				//'<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash theme-colored-icon btn-list-delete"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path></svg>';
 				'<i class="material-icons theme-colored-icon btn-list-delete">delete</i>';
-			$("#side-menu-list")[0].appendChild(newElement);
-		}
-	}
+            $("#side-menu-list")[0].appendChild(newElement);
+        }
+    }
 
-	$("#side-menu-list").children().off(changeListSelection);
-	$("#side-menu-list").children().click(changeListSelection);
-	$(".btn-list-delete").off(removeList);
-	$(".btn-list-delete").click(removeList);
+    $("#side-menu-list").children().off(changeListSelection);
+    $("#side-menu-list").children().click(changeListSelection);
+    $(".btn-list-delete").off(removeList);
+    $(".btn-list-delete").click(removeList);
 };
 
 /** Fill the list area with the currently selected list */
 let populateList = () => {
-	$("#list").empty();
-	$("#list-title")[0].innerHTML = convertToTitle(currentListName);
-	if (lists[currentListName]) {
+    $("#list").empty();
+    $("#list-title")[0].innerHTML = convertToTitle(currentListName);
+    if (lists[currentListName]) {
 
-		let keys = Object.keys(lists[currentListName]);
+        let keys = Object.keys(lists[currentListName]);
 
-		if (sortKeys) keys = keys.sort();
+        if (sortKeys) keys = keys.sort();
 
-		//Object.keys(lists[currentListName]).sort().forEach(itemName => {
-		keys.forEach(itemName => {
-			/*if (hideCompleted && lists[currentListName][itemName]) {
+        //Object.keys(lists[currentListName]).sort().forEach(itemName => {
+        keys.forEach(itemName => {
+            /*if (hideCompleted && lists[currentListName][itemName]) {
 				// Don't add
 			} else {*/
-			let newElement = document.createElement("li");
-			newElement.setAttribute("class", `list-group-item d-flex align-items-center ${lists[currentListName][itemName] && hideCompleted ? "hidden" : ""}`);
-			newElement.innerHTML = `<input type="checkbox" ${lists[currentListName][itemName] ? 'checked="true"' : ""}>` +
+            let newElement = document.createElement("li");
+            newElement.setAttribute("class", `list-group-item d-flex align-items-center ${lists[currentListName][itemName] && hideCompleted ? "hidden" : ""}`);
+            newElement.innerHTML = `<input type="checkbox" ${lists[currentListName][itemName] ? 'checked="true"' : ""}>` +
 				'<span class="checkbox"></span>' +
 				`<span class="checkbox-label" contenteditable="true">${convertToTitle(itemName)}</span>` +
 				//'<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x-circle theme-colored-icon btn-item-delete"><circle cx="12" cy="12" r="10"></circle><line x1="15" y1="9" x2="9" y2="15"></line><line x1="9" y1="9" x2="15" y2="15"></line></svg>';
 				'<i class="material-icons theme-colored-icon btn-item-delete">cancel</i>';
-			$("#list")[0].appendChild(newElement);
-			//}
-		});
+            $("#list")[0].appendChild(newElement);
+            //}
+        });
 
-		$(".checkbox").off("click", toggleCheckbox);
-		$(".checkbox").click(toggleCheckbox);
-		$(".checkbox-label").off(saveLists);
-		$(".checkbox-label").blur(saveLists);
-		$(".btn-item-delete").off(removeItem);
-		$(".btn-item-delete").click(removeItem);
-	}
+        $(".checkbox").off("click", toggleCheckbox);
+        $(".checkbox").click(toggleCheckbox);
+        $(".checkbox-label").off(saveLists);
+        $(".checkbox-label").blur(saveLists);
+        $(".btn-item-delete").off(removeItem);
+        $(".btn-item-delete").click(removeItem);
+    }
 };
 
 /** Save the lists */
 let saveLists = () => {
-	lists[currentListName] = {};
-	Array.from($("#list").children()).forEach(e => {
-		lists[currentListName][convertToVarName(e.children[2].textContent)] = e.firstChild.getAttribute("checked") === "true";
-	});
-	localStorage.setItem("toCheckLists", JSON.stringify(lists));
-	localStorage.setItem("currentList", currentListName);
+    lists[currentListName] = {};
+    Array.from($("#list").children()).forEach(e => {
+        lists[currentListName][convertToVarName(e.children[2].textContent)] = e.firstChild.getAttribute("checked") === "true";
+    });
+    localStorage.setItem("toCheckLists", JSON.stringify(lists));
+    localStorage.setItem("currentList", currentListName);
 };
 
 let main = () => {
-	// Store the Add to Home Screen prompt
-	let deferredPrompt;
-	window.addEventListener("beforeinstallprompt", e => {
-		// Prevent Chrome 67 and earlier from automatically showing the prompt
-		e.preventDefault();
-		// Stash the event so it can be triggered later.
-		deferredPrompt = e;
+    // Store the Add to Home Screen prompt
+    let deferredPrompt;
+    window.addEventListener("beforeinstallprompt", e => {
+        // Prevent Chrome 67 and earlier from automatically showing the prompt
+        e.preventDefault();
+        // Stash the event so it can be triggered later.
+        deferredPrompt = e;
 
-		if (localStorage.getItem("appInstalled") === "false") {
-			setTimeout(() => {
-				deferredPrompt.prompt();
-				localStorage.setItem("appInstalled", true);
-				deferredPrompt.userChoice.then(result => {
-					if (result.outcome === "accepted") {
-						// Installed
-					} else {
-						// Prompt dismissed
-					}
-				});
-			}, 5000);
-		}
-	});
+        if (localStorage.getItem("appInstalled") === "false") {
+            setTimeout(() => {
+                deferredPrompt.prompt();
+                localStorage.setItem("appInstalled", true);
+                deferredPrompt.userChoice.then(result => {
+                    if (result.outcome === "accepted") {
+                        // Installed
+                    } else {
+                        // Prompt dismissed
+                    }
+                });
+            }, 5000);
+        }
+    });
 
-	window["isUpdateAvailable"]
-		.then(isAvailable => {
-			if (isAvailable) {
-				$("#alert-modal").modal();
-			}
-		});
+    window["isUpdateAvailable"]
+        .then(isAvailable => {
+            if (isAvailable) {
+                $("#alert-modal").modal();
+            }
+        });
 
-	//feather.replace();
-	loadLists();
-	loadSettings();
-	populateList();
-	populateSideMenu();
+    //feather.replace();
+    loadLists();
+    loadSettings();
+    populateList();
+    populateSideMenu();
 };
 
 // #region Event handlers
--- service-worker.js
+++ service-worker.js
@@ -1,53 +1,53 @@
 /* globals importScripts */
 
 const APP_PREFIX = "ToCheck";
-const CACHE_VERSION = "_v1.15";
+const CACHE_VERSION = "_v1.16";
 const CACHE_NAME = APP_PREFIX;
 const URLS = [
-	"./",
-	"./index.html",
-	"./styles/style.css",
-	"./styles/boostrap.min.css",
-	"https://fonts.googleapis.com/css?family=Open+Sans",
-	"https://fonts.googleapis.com/icon?family=Material+Icons",
-	"./js/index.js",
-	"https://code.jquery.com/jquery-3.3.1.slim.min.js",
-	"./3rd-party/bootstrap.bundle.min.js",
-	"./android-chrome-192x192.png",
-	"./android-chrome-384x384.png",
-	"./android-chrome-512x512.png",
-	"./apple-touch-icon.png",
-	"./favicon-16x16.png",
-	"./favicon-32x32.png",
-	"./mstile-150x150.png",
-	"./safari-pinned-tab.svg",
-	"./favicon.ico",
+    "./",
+    "./index.html",
+    "./styles/style.css",
+    "./styles/boostrap.min.css",
+    "https://fonts.googleapis.com/css?family=Open+Sans",
+    "https://fonts.googleapis.com/icon?family=Material+Icons",
+    "./js/index.js",
+    "https://code.jquery.com/jquery-3.3.1.slim.min.js",
+    "./3rd-party/bootstrap.bundle.min.js",
+    "./android-chrome-192x192.png",
+    "./android-chrome-384x384.png",
+    "./android-chrome-512x512.png",
+    "./apple-touch-icon.png",
+    "./favicon-16x16.png",
+    "./favicon-32x32.png",
+    "./mstile-150x150.png",
+    "./safari-pinned-tab.svg",
+    "./favicon.ico",
 ];
 
 importScripts("js/cache-polyfill.js");
 
 // Cache resources
 self.addEventListener("install", function (e) {
-	e.waitUntil(
-		caches.open(CACHE_NAME).then(function (cache) {
-			return cache.addAll(URLS);
-		})
-	);
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(function (cache) {
+            return cache.addAll(URLS);
+        })
+    );
 });
 
 // Respond with cached resources
 self.addEventListener("fetch", function (e) {
-	//console.log(e.request.url);	// eslint-disable-line no-console
-	e.respondWith(
-		caches.match(e.request).then(function (response) {
-			// If valid response is found in cache return it, otherwise,
-			// fetch from the Internet and put in cache
-			return response || fetch(e.request).then(function (r) {
-				return caches.open(CACHE_NAME).then(function (cache) {
-					cache.put(e.request, r.clone());
-					return response;
-				});
-			});
-		})
-	);
+    //console.log(e.request.url);	// eslint-disable-line no-console
+    e.respondWith(
+        caches.match(e.request).then(function (response) {
+            // If valid response is found in cache return it, otherwise,
+            // fetch from the Internet and put in cache
+            return response || fetch(e.request).then(function (r) {
+                return caches.open(CACHE_NAME).then(function (cache) {
+                    cache.put(e.request, r.clone());
+                    return response;
+                });
+            });
+        })
+    );
 });
